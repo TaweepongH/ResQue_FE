deee80207d3c374d91e967f3c77ad094fe89364f
Joling Weng <joling6027@gmail.com>
2023-06-19 11:58:52 -0700

    Configured header and navigation

--- client/ResQue_Frontend/App.tsx
+++ client/ResQue_Frontend/App.tsx
@@ -6,17 +6,21 @@
  */
 
 import React, { useState } from 'react';
-import Navbar from './Components/Navbar/Navbar';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './Components/Login';
+import TabNavigator from './Components/TabNavigator/TabNavigator'
+import StackNavigator from './Components/StackNavigator/StackNavigator';
 
 function App(): JSX.Element {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
     <NavigationContainer>
-      {/* TODO: Need to handle setIsLoggedIn based on authentication. Skip this for now.*/}
-      {isLoggedIn ? <Navbar /> : <Login />}
+        {/* TODO: Need to handle setIsLoggedIn based on authentication. Skip this for now.*/}
+        {isLoggedIn ? (<>
+          <TabNavigator />
+        </>) : (
+          <StackNavigator />
+        )}
     </NavigationContainer>
   );
 }
