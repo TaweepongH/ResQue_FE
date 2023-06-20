d43a22f1f16847718a489431a5fd4785cbe00603
Ari Choi <choiari1002@gmail.com>
2023-06-19 17:33:56 -0700

    Update index.js

--- client/ResQue_Frontend/index.js
+++ client/ResQue_Frontend/index.js
@@ -6,7 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Login from './Components/Login';
 import {name as appName} from './app.json';
-import Main from './Components/Main';
 
 AppRegistry.registerComponent(appName, () => Login);
-AppRegistry.registerComponent(appName, () => Main);
