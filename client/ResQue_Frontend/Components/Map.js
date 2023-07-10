5c7e2ad47b0e8c824c49badad5f72dcd3407bf7c
Reid Gibson-Bingham <reidgibson-bingham@Reids-MacBook-Air.local>
2023-07-09 20:45:26 -0700

    The device location seems to be working for ios, but i haven't tested it for Android.

--- client/ResQue_Frontend/Components/Map.js
+++ client/ResQue_Frontend/Components/Map.js
@@ -1,5 +1,5 @@
 
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, PermissionsAndroid } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
