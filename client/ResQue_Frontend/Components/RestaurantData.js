93ecfaf03e9afa6f4e6f730c698dcc6219af7584
Reid Gibson-Bingham <reidgibson-bingham@Reids-MacBook-Air.local>
2023-07-14 16:25:01 -0700

    displaying more restaurant data and trying to send it to the our partners database via a post request. currently facing problems with missing required fields.

--- client/ResQue_Frontend/Components/RestaurantData.js
+++ client/ResQue_Frontend/Components/RestaurantData.js
@@ -24,9 +24,11 @@ const RestaurantData = () => {
           .then(data => {
 
             for (let i = 0; i < 20; i++) {
-                console.log(data['businesses'][i]);
+                // console.log(data['businesses'][i]);
                 addData(data['businesses'][i])
             }
+            
+            makePostRequests(data['businesses']);
             
           })
           .catch(error => {
@@ -35,10 +37,69 @@ const RestaurantData = () => {
           });
       }, []);
 
+      useEffect(() => {
+        console.log(`data test: 
+          firstName: "N/A",
+          lastName: "N/A",
+          companyName: ${restaurantData.length > 0 ? restaurantData[0].name : 'N/A'},
+          email: ${restaurantData.length > 0 ? restaurantData[0].url : 'N/A'},
+          password: "12345678",
+          phone: ${restaurantData.length > 0 ? restaurantData[0].phone : 'N/A'},
+          address: [${restaurantData.length > 0 ? restaurantData[0].location['address1'] : 'N/A'}, ${restaurantData.length > 0 ? restaurantData[0].location['city'] : 'N/A'}, ${restaurantData.length > 0 ? restaurantData[0].location['state'] : 'N/A'}, ${restaurantData.length > 0 ? restaurantData[0].location['country'] : 'N/A'}],
+          genre: ${restaurantData.length > 0 ? restaurantData[0].categories[0].title : 'N/A'},`
+        );
+      }, [makePostRequests]);
+
+      const makePostRequests = (businesses) => {
+        businesses.forEach(restaurant => {
+          fetch('https://app-57vwexmexq-uc.a.run.app/api/partners/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkF2MGE3ZlBTWjVpcE9aNE9jMGQzIiwicm9sZSI6InVzZXIiLCJpYXQiOjE2ODkzNzYyMDYsImV4cCI6MTY4OTM3OTgwNn0.io9GX3EXebr7Z7MVrATzj5YGnkjZzPfGvdISO-KmcJ0',
+            },
+            body: JSON.stringify({
+              firstName: 'John',
+              lastName: 'Johnson',
+              companyName: restaurant.name,
+              email: restaurant.url,
+              password: '12345678',
+              phone: restaurant.phone,
+              address: [
+                restaurant.location['address1'],
+                restaurant.location['city'],
+                restaurant.location['state'],
+                restaurant.location['country']
+              ],
+              genre: restaurant.categories[0].title,
+            }),
+          })
+            .then(response => response.json())
+            .then(result => {
+              console.log('post request result: ', result);
+            })
+            .catch(error => {
+              console.error('post request error: ', error);
+            });
+        });
+      };
+    
+
   return (
     <>
         {restaurantData.map((restaurant, index) => (
-      <Text style={styles.container} key={index}>Name: {restaurant.name}</Text>
+      
+      <>
+        <Text style={styles.container} key={index}> 
+
+                Name: {restaurant.name}, location: {restaurant.location['address1']}, rating: {restaurant.rating}
+        </Text>
+
+        <Text>
+                genre: {restaurant['categories'][0].title}, price: {restaurant.price}, phone: {restaurant.phone}
+        </Text>
+      </>
+
     ))}
     </>
   )
