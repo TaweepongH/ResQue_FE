dc68ee333a90eb17e896af6d3a71a0bcd7ef2486
Reid Gibson-Bingham <reidgibson-bingham@Reids-MacBook-Air.local>
2023-07-14 01:33:12 -0700

    displaying restaurnat Data fetched from the yelp fusion api.

--- client/ResQue_Frontend/App.tsx
+++ client/ResQue_Frontend/App.tsx
@@ -10,6 +10,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import TabNavigator from './Components/TabNavigator/TabNavigator'
 import StackNavigator from './Components/StackNavigator/StackNavigator';
+import RestaurantData from './Components/RestaurantData';
 
 
 function App(): JSX.Element {
@@ -17,16 +18,17 @@ function App(): JSX.Element {
   const [user, setUser] = useState(true);
 
   return (
-    <NavigationContainer>
+    // <NavigationContainer>
 
-        {/* TODO: Need to handle setIsLoggedIn based on authentication. Skip this for now.*/}
-      {user ? (<>
-          <TabNavigator />
-        </>) : (
-          <StackNavigator />
-        )}
+    //     {/* TODO: Need to handle setIsLoggedIn based on authentication. Skip this for now.*/}
+    //   {user ? (<>
+    //       <TabNavigator />
+    //     </>) : (
+    //       <StackNavigator />
+    //     )}
 
-    </NavigationContainer>
+    // </NavigationContainer>
+    <RestaurantData></RestaurantData>
   );
 }
 
--- client/ResQue_Frontend/Components/RestaurantData.js
+++ client/ResQue_Frontend/Components/RestaurantData.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-const { Text } = require("react-native");
+const { Text, StyleSheet } = require("react-native");
 const apiKey = 'nEEEhLarJcEvFiV6K2h1Pp_M3fMYx5whC4xkYbXuYrmpmMxsLXH0O1sCdlZ9B30B83v2GBU08y3XRwSMSqHXzFgCLZLs52Pe4_VfersINvhg9X9F7NM4hDTrnYysZHYx';                
 const searchTerm = 'restaurants'; // Or any other search term
 const location = 'Vancouver';
@@ -8,6 +8,10 @@ const location = 'Vancouver';
 const RestaurantData = () => {
 
     const [restaurantData, setRestaurantData] = useState([]);
+
+    const addData = (newData) => {
+        setRestaurantData(prevData => [...prevData, newData]);
+    };
 
     useEffect(() => {
         fetch(`https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${location}`, {
@@ -18,8 +22,12 @@ const RestaurantData = () => {
         })
           .then(response => response.json())
           .then(data => {
-            // Handle the response data
-            console.log("data ",data);
+
+            for (let i = 0; i < 20; i++) {
+                console.log(data['businesses'][i]);
+                addData(data['businesses'][i])
+            }
+            
           })
           .catch(error => {
             // Handle error
@@ -29,11 +37,23 @@ const RestaurantData = () => {
 
   return (
     <>
-      {}
+        {restaurantData.map((restaurant, index) => (
+      <Text style={styles.container} key={index}>Name: {restaurant.name}</Text>
+    ))}
     </>
   )
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#FEEEEF',
+      paddingBottom: 20,
+    },
+
+  });
 
 export default RestaurantData;
 
