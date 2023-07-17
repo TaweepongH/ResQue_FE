3a8a9987f9fb039e6e850e8e98e81aa84f75369a
Reid Gibson-Bingham <reidgibson-bingham@Reids-MacBook-Air.local>
2023-07-17 15:08:20 -0700

    updated the restaurantData helper function to make a second call to the yelp api to include store hours and days of operation. the process to write the data to our db isn't as elegant or efficient as it should be, but it retrieves and writes data. I will optimize it in the future.

--- client/ResQue_Frontend/Components/RestaurantData.js
+++ client/ResQue_Frontend/Components/RestaurantData.js
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react";
 const { Text, StyleSheet } = require("react-native");
 const apiKey = 'nEEEhLarJcEvFiV6K2h1Pp_M3fMYx5whC4xkYbXuYrmpmMxsLXH0O1sCdlZ9B30B83v2GBU08y3XRwSMSqHXzFgCLZLs52Pe4_VfersINvhg9X9F7NM4hDTrnYysZHYx';                
 const searchTerm = 'restaurants'; // Or any other search term
-const location = 'Vancouver';
+const location = 'Vancouver'; 
+let operationalDays = [];
+
+
+// this process is not as automated as it should be, I ended up having to make two seperate api calls to get all the necessary data for one partner document, i will try to make it more efficient in the future
 
 
 const RestaurantData = () => {
@@ -14,6 +18,7 @@ const RestaurantData = () => {
     };
 
     useEffect(() => {
+        setRestaurantData([]);
         fetch(`https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${location}`, {
           method: 'GET',
           headers: {
@@ -23,13 +28,19 @@ const RestaurantData = () => {
           .then(response => response.json())
           .then(data => {
 
-            for (let i = 0; i < 20; i++) {
-                // console.log(data['businesses'][i]);
-                addData(data['businesses'][i])
-            }
-            
-            makePostRequests(data['businesses']);
-            
+            // console.log("restaurant data: ", data['businesses']);
+
+            // making sure the days array is empty before calling getHoursOfOperation, because that function requires an empty array to accuratlely record the days
+            operationalDays = [];
+
+            let num = 0
+
+            addData(data['businesses'][num]);
+
+            getHoursOfOperation(data['businesses'][num])
+    
+            makePostRequests(restaurantData)
+        
           })
           .catch(error => {
             // Handle error
@@ -37,80 +48,111 @@ const RestaurantData = () => {
           });
       }, []);
 
-      useEffect(() => {
-        console.log(`data test: 
-          firstName: "N/A",
-          lastName: "N/A",
-          companyName: ${restaurantData.length > 0 ? restaurantData[0].name : 'N/A'},
-          email: ${restaurantData.length > 0 ? restaurantData[0].url : 'N/A'},
-          password: "12345678",
-          phone: ${restaurantData.length > 0 ? restaurantData[0].phone : 'N/A'},
-          address: [${restaurantData.length > 0 ? restaurantData[0].location['address1'] : 'N/A'}, ${restaurantData.length > 0 ? restaurantData[0].location['city'] : 'N/A'}, ${restaurantData.length > 0 ? restaurantData[0].location['state'] : 'N/A'}, ${restaurantData.length > 0 ? restaurantData[0].location['country'] : 'N/A'}],
-          genre: ${restaurantData.length > 0 ? restaurantData[0].categories[0].title : 'N/A'},`
-        );
-      }, [makePostRequests]);
+      const getHoursOfOperation = (restaurant) => {
+
+        // businesses.forEach(restaurant => {
+
+            
+
+                fetch(`https://api.yelp.com/v3/businesses/${restaurant.id}`, {
+                    method: 'GET',
+                    headers: {
+                    Authorization: `Bearer ${apiKey}`,
+                    },
+                })
+                .then(response => response.json())
+                .then(data => {
+                    
+                    data.hours[0]['open'].forEach((day, index) => {
+                        
+                        if (index > 0) {
+                            const previousDay = data.hours[0]['open'][index - 1].day;
+                            const currentDay = day.day;
+                    
+                            if (currentDay === previousDay) {
+                                console.log("Current day is the same as the previous day.");
+                            } else {
+                               operationalDays.push(currentDay);
+                            }
+                        } else {
+                            operationalDays.push(day.day);
+                        }
+
+                    })
+
+                    console.log("operational days for this restaurant: ", operationalDays);
+
+                    
+                const updatedRestaurant = {
+                    ...restaurant,
+                    daysOfOperation: operationalDays,
+                    hoursOfOperation: data.hours[0]['open']
+                };
+
+                // Update the restaurant data in state with the hours of operation
+                setRestaurantData(prevData => prevData.map(prevRestaurant => prevRestaurant.id === restaurant.id ? updatedRestaurant : prevRestaurant));
+
+                console.log("restaurant Data from the getHoursOfOperation function: ", restaurantData);
+
+                })
+                .catch(error => {
+                    // Handle error
+                    console.error(error);
+                });
+            
+      };
 
       const makePostRequests = (businesses) => {
 
-        businesses.forEach(restaurant => {
+            businesses.forEach(restaurant => {
 
-            const { name, url, phone, location, categories } = restaurant;
-
-            const address = [
-            location.address1,
-            location.city,
-            location.state,
-            location.country
-            ];
-
-            const payload = {
-            firstName: 'John',
-            lastName: 'Johnson',
-            companyName: name,
-            email: url,
-            password: '12345678',
-            phone: phone,
-            address: address,
-            operationDate: "Mon,Tue,Wed,Thur,Fri",
-            operationTime: ["10.00-11.30"],
-            genre: categories[0].title
-            };
-
-          fetch('https://app-57vwexmexq-uc.a.run.app/api/partners/register', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkF2MGE3ZlBTWjVpcE9aNE9jMGQzIiwicm9sZSI6InVzZXIiLCJpYXQiOjE2ODkzODQ2NjYsImV4cCI6MTY4OTM4ODI2Nn0.CRd0C_QxMaKGQsZ6QK7FjHcQiTGWwrPCy0ZhU6_8yjQ',
-            },
-            body: JSON.stringify(payload),
-          })
-            .then(response => response.json())
-            .then(result => {
-              console.log('post request result: ', result);
-            })
-            .catch(error => {
-              console.error('post request error: ', error);
+                const { name, url, phone, location, categories, image_url, daysOfOperation, hoursOfOperation } = restaurant;
+    
+                const address = [
+                location.address1,
+                location.city,
+                location.state,
+                location.country
+                ];
+    
+                const payload = {
+                firstName: 'N/A',
+                lastName: 'N/A',
+                companyName: name,
+                email: url,
+                password: '12345678',
+                phone: phone,
+                address: address,
+                operationDate: daysOfOperation,
+                operationTime: hoursOfOperation,
+                genre: categories[0].title, 
+                images: [image_url]
+                };
+    
+              fetch('https://app-57vwexmexq-uc.a.run.app/api/partners/register', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkF2MGE3ZlBTWjVpcE9aNE9jMGQzIiwicm9sZSI6InVzZXIiLCJpYXQiOjE2ODkzODQ2NjYsImV4cCI6MTY4OTM4ODI2Nn0.CRd0C_QxMaKGQsZ6QK7FjHcQiTGWwrPCy0ZhU6_8yjQ',
+                },
+                body: JSON.stringify(payload),
+              })
+                .then(response => response.json())
+                .then(result => {
+                  console.log('post request result: ', result);
+                })
+                .catch(error => {
+                  console.error('post request error: ', error);
+                });
             });
-        });
+
+        
       };
     
 
   return (
     <>
-        {restaurantData.map((restaurant, index) => (
-      
-      <>
-        <Text style={styles.container} key={index}> 
-
-                Name: {restaurant.name}, location: {restaurant.location['address1']}, rating: {restaurant.rating}
-        </Text>
-
-        <Text>
-                genre: {restaurant['categories'][0].title}, price: {restaurant.price}, phone: {restaurant.phone}
-        </Text>
-      </>
-
-    ))}
+    
     </>
   )
 
