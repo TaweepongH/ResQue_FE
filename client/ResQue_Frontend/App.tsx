d6a2dfa00ad54230bb157e5b291f7250e0913ca2
Reid Gibson-Bingham <reidgibson-bingham@Reids-MacBook-Air.local>
2023-07-20 15:06:16 -0700

    replacing the contents of the package-lock.json file with the ones from the main branch and cleaning the app.tsx file.

--- client/ResQue_Frontend/App.tsx
+++ client/ResQue_Frontend/App.tsx
@@ -10,8 +10,6 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import TabNavigator from './Components/TabNavigator/TabNavigator'
 import StackNavigator from './Components/StackNavigator/StackNavigator';
-import LoginEmail from './Components/LoginEmail';
-import Login from './Components/Login';
 
 
 function App(): JSX.Element {
@@ -19,17 +17,18 @@ function App(): JSX.Element {
   const [user, setUser] = useState(true);
 
   return (
-    // <NavigationContainer>
 
-    //     {/* TODO: Need to handle setIsLoggedIn based on authentication. Skip this for now.*/}
-    //   {user ? (<>
-    //       <TabNavigator />
-    //     </>) : (
-    //       <StackNavigator />
-    //     )}
+    <NavigationContainer>
 
-    // </NavigationContainer>
-    <LoginEmail></LoginEmail>
+        {/* TODO: Need to handle setIsLoggedIn based on authentication. Skip this for now.*/}
+      {user ? (<>
+          <TabNavigator />
+        </>) : (
+          <StackNavigator />
+        )}
+
+    </NavigationContainer>
+    
   );
 }
 
