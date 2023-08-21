bd7e87e0011b194e584c1f2cd68c02752d80f596
Reid Gibson-Bingham <reidgibson-bingham@Reids-MacBook-Air.local>
2023-08-21 16:20:00 -0700

    making a fetch function in the editPrifle.js component to retrieve the current user data. Currently unable to fetch the data because i'm not authorized to see it with the login bearer token.

--- client/ResQue_Frontend/screens/myinfo/EditProfile.js
+++ client/ResQue_Frontend/screens/myinfo/EditProfile.js
@@ -2,7 +2,34 @@ import React from 'react';
 import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import IconMat from 'react-native-vector-icons/MaterialIcons';
 
+const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijl2Tk1TNFF1WWRBaFRnbWZJNm1yIiwicm9sZSI6InBhcnRuZXIiLCJpYXQiOjE2OTI2NTg4NjgsImV4cCI6MTY5MjY2MjQ2OH0.E7STxca_FUvuJ5arjIbCcW_oS3CoQ4gXXNpEf420rFw'
+
 const EditProfile = () => {
+
+    const retrieveCurrentUserData = () => {
+        fetch('https://app-57vwexmexq-uc.a.run.app/api/users/current', {
+            method: 'GET', 
+
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                Authorization: `Bearer ${bearerToken}`
+            }
+        })
+        .then((response) => response.text())
+        .then((data) => {
+            if (data) {
+                console.log("there is data! it is: ", data);
+            }
+        })
+        .catch((error) => {
+            console.log("error is: ", error);
+        })
+    }
+
+    useState( () => {
+        retrieveCurrentUserData();
+    }, []);
+
     return (
         <View style={styles.container}>
             <View style={styles.user}>
