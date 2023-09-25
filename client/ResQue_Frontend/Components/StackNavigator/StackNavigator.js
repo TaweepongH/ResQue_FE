5b29364abfcaaa65edbd0f11e9cb87ab71d037a7
ReidGibson-Bingham <jsr3b0rn@gmail.com>
2023-09-25 15:27:23 -0700

    added missing imports to out stacknavigator file.

--- client/ResQue_Frontend/Components/StackNavigator/StackNavigator.js
+++ client/ResQue_Frontend/Components/StackNavigator/StackNavigator.js
@@ -12,6 +12,9 @@ import EditProfile from '../../screens/myinfo/EditProfile';
 import GoogleAuth from '../GoogleAuth';
 import RestaurantInfo from '../RestaurantInfo';
 import QueueConfirm from '../../screens/QueueConfirm';
+import Settings from '../../screens/Settings';
+import Feedback from '../Feedback';
+import TermsPolicies from '../../screens/more/TermsPolicies';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,24 +22,83 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator
       screenOptions={{  
+        headerStyle: {
+          backgroundColor: 'white',
+          height: 110, 
+        },
         headerTitleStyle: {
           fontWeight: 'bold',
+          fontSize: 24,
         },
+        headerTintColor: 'black',
         headerTitleAlign: 'center',
+        // headerBackTitleVisible: false, // not working
+        headerBackTitleStyle: {fontSize: 0} 
       }}
     >
-      <Stack.Screen name='Login' component={Login} />
-      <Stack.Screen name='LoginEmail' component={LoginEmail} />
-      <Stack.Screen name='GoogleAuth' component={GoogleAuth} />
-      <Stack.Screen name='Register' component={Register} />
-      <Stack.Screen name='ResetPwd' component={ResetPwd} />
-      <Stack.Screen name='OtpVerify' component={OtpVerify} />
-      <Stack.Screen name='CreateNewPwd' component={CreateNewPwd} />
-      <Stack.Screen name='PwdResetComplete' component={PwdResetComplete} />
+      <Stack.Screen 
+        name='Login' 
+        component={Login} 
+        options={{
+          headerShown: false,
+        }}
+        />
+      <Stack.Screen 
+        name='LoginEmail' 
+        component={LoginEmail} 
+        options={{
+          headerTitle: 'Login',
+        }}
+        />
+      <Stack.Screen 
+        name='GoogleAuth' 
+        component={GoogleAuth} 
+        options={{
+          headerShown: false,
+        }}
+        />
+      <Stack.Screen
+        name='Register' 
+        component={Register}
+        options={{
+          headerTitle: 'Register',
+        }}
+      />
+      <Stack.Screen 
+        name='ResetPwd' 
+        component={ResetPwd}
+        options={{
+          headerTitle: 'Reset Password',
+        }}
+        />
+      <Stack.Screen 
+        name='OtpVerify' 
+        component={OtpVerify} 
+        options={{
+          headerTitle: 'Reset Password',
+        }}
+      />         
+      <Stack.Screen 
+        name='CreateNewPwd' 
+        component={CreateNewPwd}
+        options={{
+          headerTitle: 'Reset Password',
+        }}
+        />
+      <Stack.Screen 
+        name='PwdResetComplete' 
+        component={PwdResetComplete} 
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name='Settings' component={Settings} />
+      <Stack.Screen name='Feedback' component={Feedback} />
+      <Stack.Screen name='TermsPolicies' component={TermsPolicies} />
       <Stack.Screen name='EditProfile' component={EditProfile} />
       <Stack.Screen name='RestaurantInfo' component={RestaurantInfo} />
       <Stack.Screen name='QueueConfirm' component={QueueConfirm} />
-      
+
     </Stack.Navigator>
   )
 }
